906803a2233c154b7281053ce2fad85b7e8873c6
vives <vsvivevives@gmail.com>
2019-08-10 22:11:41 +0530

    Update server.js
    port added

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
